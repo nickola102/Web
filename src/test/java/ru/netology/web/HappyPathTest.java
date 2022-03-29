24e3063cdc943041b308bfa3007201592e22da79
nickola102 <nickola@list.ru>
2022-03-29 16:26:45 +0300

    update test

--- src/test/java/ru/netology/web/HappyPathTest.java
+++ src/test/java/ru/netology/web/HappyPathTest.java
@@ -1,11 +1,21 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class HappyPathTest {
     @Test
     void shouldSubmitRequest() {
         open("http://localhost:9999");
-        SelenadeElement form = $("[data-test-id=callback-form]");
+        SelenideElement form = $("[data-test-id=callback-form]");
+        form.$("[data-test-id=name] input").setValue("Игорь");
+        form.$("[data-test-id=tel] input").setValue("+79998887766");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[data-test-id=submit]").click();
+        $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в блтжайшее время."));
     }
 }
